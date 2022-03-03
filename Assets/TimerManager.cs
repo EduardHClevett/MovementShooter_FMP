0c2e178a74b8de1bd75edc5e1dd4616cdad83fca
DisortingLines <ehorandiclevett@gmail.com>
2022-03-03 11:17:57 +0000

    Timer functionality started
    Just needs linking to everything

---
+++ Assets/TimerManager.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimerManager : MonoBehaviour
+{
+    public static TimerManager instance;
+
+    public TextMeshProUGUI timerTxt;
+
+    TimeSpan currentTime;
+
+    bool timerGoing;
+    private float elapsedTime;
+
+    private void Awake()
+    {
+        instance = this;
+
+        if (instance != this)
+            Destroy(this);
+    }
+
+    private void Start()
+    {
+        timerGoing = false;
+
+        timerTxt.enabled = false;
+    }
+
+    public void BeginTimer()
+    {
+        timerGoing = true;
+
+        timerTxt.enabled = true;
+
+        elapsedTime = 0;
+
+        StartCoroutine(UpdateTimer());
+    }
+
+    public void EndTimer()
+    {
+        timerGoing = false;
+
+        StartCoroutine(HideTimerTxt());
+    }
+
+    private IEnumerator UpdateTimer()
+    {
+        while (timerGoing)
+        {
+            elapsedTime += Time.deltaTime;
+
+            currentTime = TimeSpan.FromSeconds(elapsedTime);
+
+            string timeStr = currentTime.ToString("mm':'ss'.'ff");
+
+            yield return null;
+        }
+    }
+
+    private IEnumerator HideTimerTxt()
+    {
+        yield return new WaitForSeconds(8f);
+
+        timerTxt.enabled = false;
+    }
+
+    public void FinishTimer()
+    {
+
+    }
+
+    public void CancelTimer()
+    {
+
+    }
+}
